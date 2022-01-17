c59b94bb0186d1ecdfc47c458833b9c60c0b852e
Jacob <jacob@Jacob.local>
2022-01-18 00:09:26 +0800

    commit

---
+++ lib/listItem.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+class ListItem extends StatelessWidget {
+  const ListItem() : super();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        height: 50,
+        child:Text('123')
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import './listItem.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+        // This makes the visual density adapt to the platform that you run
+        // the app on. For desktop platforms, the controls will be smaller and
+        // closer together (more dense) than on mobile platforms.
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage ({ Key key,this.title }) : super(key: key);
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(title),
+      ),
+      body: ListView.builder(
+        physics: const AlwaysScrollableScrollPhysics(),
+        itemBuilder:(BuildContext context,int index){
+        return ListItem();
+      },
+      itemCount: 50,),// This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
